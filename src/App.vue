36e1df896ed5872e402eb7062c54799a81056924
Andres <andresdomlez@gmail.com>
2020-05-10 23:39:58 +0100

    components creation, new quote creation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <NewQuote @quoteAdded="newQuote"></NewQuote>
+    <QuoteGrid :quotes="quotes"></QuoteGrid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
+export default {
+  data() {
+    return {
+      quotes: ["Just a quote to see something"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    }
+  },
+  components: {
+    QuoteGrid,
+    NewQuote
+  }
+};
 </script>
 
 <style></style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <Quote v-for="quote in quotes" :key="quote">{{ quote }}</Quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote";
+
+export default {
+  props: ["quotes"],
+  components: { Quote }
+};
+</script>
+
+<style></style>
